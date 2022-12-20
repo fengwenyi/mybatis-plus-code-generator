b37448923f3399e7d06d32e94bdf7f0470b4645a
ErwinFeng <xfsy_2015@163.com>
2022-12-20 16:34:54 +0800

    修改一些没有改的问题

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:17-jdk-alpine
+FROM openjdk:8-jdk-alpine
 MAINTAINER Erwin Feng xfsy_2015@163.com
 COPY target/mybatis-plus-code-generator-*.jar mybatis-plus-code-generator.jar
 ENTRYPOINT ["sh", "-c", "java -jar mybatis-plus-code-generator.jar"]
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 version=`awk '/<version>[^<]+<\/version>/{gsub(/<version>|<\/version>/,"",$1);print $1;exit;}' pom.xml`
 echo $version
-jdk17
+#jdk17
 mvn clean package -DskipTests
 docker build -t fengwenyi/mybatis-plus-code-generator .
 docker tag fengwenyi/mybatis-plus-code-generator fengwenyi/mybatis-plus-code-generator
